4759e043d20bb5fe42f029e01214bd49be63eded
Tyler Gass <tgx@tohaccian.net>
2013-05-29 12:09:22 -0400

    Fixed minor thing for setting the hostname with a random value if you rDNS

--- 01-ubuntu12.02-puppetmaster/puppetize_1204.sh
+++ 01-ubuntu12.02-puppetmaster/puppetize_1204.sh
@@ -5,6 +5,11 @@ sudo apt-get update
 sudo apt-get install puppetmaster -y
 VM_HOSTNAME=$(dig +short -x $(ifconfig eth1 | awk '/inet addr:/{gsub(/addr:/,"",$2);print $2}'));
 VM_HOSTNAME=${VM_HOSTNAME%%.};
-sudo echo $VM_HOSTNAME > /etc/hostname
-sudo stop hostname #not needed
-sudo start hostname
+if [ "$VM_HOSTNAME" == "" ];
+then
+  VM_HOSTNAME=host$RANDOM
+else
+  sudo echo $VM_HOSTNAME > /etc/hostname
+  sudo stop hostname #not needed
+  sudo start hostname
+fi
